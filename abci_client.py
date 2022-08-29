1a5a66439f6fef241bb320a484f09579256b0003
Philip Offtermatt <p.offtermatt@gmail.com>
2022-08-29 11:08:20 +0200

    Regenerate code and start interfacing with grpc

--- abci_client.py
+++ abci_client.py
@@ -1,7 +1,20 @@
+from datetime import datetime
 import grpc
+import tendermint.abci.types_pb2 as atypes
 import tendermint.abci.types_pb2_grpc as abci
 
 
 if __name__ == '__main__':
     with grpc.insecure_channel('localhost:9091') as channel:
         stub = abci.ABCIApplicationStub(channel)
+        request = atypes.RequestInitChain()
+        # request.time = datetime.now().time()
+        request.chain_id = "tendermock"
+
+        validators = [
+            atypes.ValidatorUpdate(pub_key = (5000 + i).to_bytes(2, byteorder="big"), power = i) for i in range(0, 5)
+            ]
+
+        request.validators = validators        
+
+        response = stub.InitChain()
